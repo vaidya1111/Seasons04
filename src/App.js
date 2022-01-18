b8a0ecb49a57f1299f6934f30550be3a8737018f
vaidya1111 <sagar.vaidya11@gmail.com>
2022-01-18 17:31:14 +0530

    seasons commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    // function App() {
+    //   window.navigator.geolocation.getCurrentPosition(
+    //     (position)=> console.log(position), //success call back
+    //     (err)=>console.log(err) // failure call back
+    //   );
+
+    //   return (
+    //     <div className="App">
+    //     Hello
+    //     </div>
+    //   );
+    // }
+class App extends React.Component{
+
+  // constructor(props){ //props same as functional component props
+  //   super(props);
+  //   this.state={lat:null,errMsg:''};  
+  // }
+
+  state={lat:null} //Alternate state initialization and the babel converts this code into the above constructor code.
+
+  componentDidMount(){
+    window.navigator.geolocation.getCurrentPosition(
+      (position)=> {
+        //setState comes from React.Component
+         //success call back
+        this.setState({lat: position.coords.latitude})
+      },
+      (err)=> {
+        // failure call back
+        this.setState({errMsg:err.message});
+      } 
+    );
+  }
+
+  renderContent(){
+    if(this.state.lat && !this.state.errMsg){
+      return <SeasonDisplay lat={this.state.lat}/>
+    }
+    if(!this.state.lat && this.state.errMsg){
+      return <div>Error :{this.state.errMsg} </div>
+    }
+    return <Spinner text="Please Enable the Location "/>
+  }
+  
+//React says we have to define render
+  render(){
+      return (
+      <div className="border-red">
+        {this.renderContent()}
+      </div>)
+  }
 }
-
 export default App;
